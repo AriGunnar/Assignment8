990c0647bec343db12fa2590195162426436e49a
ingo00 <ibe15@hi.is>
2023-03-07 11:06:30 +0000

    Added Author.java Added FacultyMember.java Added User.java Added Book.java

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.List;
+
+public class Book {
+    String title;
+
+    List<Author> authors;
+
+    public Book(String title, List<Author> authors) {
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void addAuthor(Author author) {
+    }
+}
