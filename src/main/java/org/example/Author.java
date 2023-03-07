9d8c9da7502c6019010528e96de01fef28e7bdb0
ingo00 <ibe15@hi.is>
2023-03-07 10:36:18 +0000

    Added Author.java

---
+++ src/main/java/org/example/Author.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Author {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
